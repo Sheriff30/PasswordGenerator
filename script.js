9843a8b11620c48db58065f7abc374677a4cdfd1
Sheriff30 <Sherifdevv@gmail.com>
2023-08-31 13:15:01 +0100

    init

---
+++ script.js
@@ -0,0 +1,12 @@
+const slider = document.querySelector(".range");
+const sliderVal = document.querySelector(".charsetOutput");
+
+slider.addEventListener("mousemove", function () {
+  // Progress bar
+  const x = ((this.value - this.min) / (this.max - this.min)) * 100;
+  console.log(x);
+  const color = `linear-gradient(90deg , rgb(164,255,175) ${x}% , rgb(24,23,31) ${x}%)`;
+  this.style.background = color;
+  // value change
+  sliderVal.textContent = this.value;
+});
