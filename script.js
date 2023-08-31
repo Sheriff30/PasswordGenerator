7a799c862f8ac7707ef7c83185bf912287d47612
Sheriff30 <Sherifdevv@gmail.com>
2023-08-31 13:38:25 +0100

    updateds

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const slider = document.querySelector(".range");
 const sliderVal = document.querySelector(".charsetOutput");
 
-slider.addEventListener("mousemove", function () {
+const sliderEvent = function () {
   // Progress bar
   const x = ((this.value - this.min) / (this.max - this.min)) * 100;
   console.log(x);
@@ -9,13 +9,6 @@ slider.addEventListener("mousemove", function () {
   this.style.background = color;
   // value change
   sliderVal.textContent = this.value;
-});
-slider.addEventListener("change", function () {
-  // Progress bar
-  const x = ((this.value - this.min) / (this.max - this.min)) * 100;
-  console.log(x);
-  const color = `linear-gradient(90deg , rgb(164,255,175) ${x}% , rgb(24,23,31) ${x}%)`;
-  this.style.background = color;
-  // value change
-  sliderVal.textContent = this.value;
-});
+};
+slider.addEventListener("mousemove", sliderEvent);
+slider.addEventListener("touchmove", sliderEvent);
