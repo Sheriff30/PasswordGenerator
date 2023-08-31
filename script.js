2416b0c5edf2612b99dbf26d1379b95665be9a61
Sheriff30 <Sherifdevv@gmail.com>
2023-08-31 13:21:44 +0100

    update

--- script.js
+++ script.js
@@ -10,3 +10,12 @@ slider.addEventListener("mousemove", function () {
   // value change
   sliderVal.textContent = this.value;
 });
+slider.addEventListener("click", function () {
+  // Progress bar
+  const x = ((this.value - this.min) / (this.max - this.min)) * 100;
+  console.log(x);
+  const color = `linear-gradient(90deg , rgb(164,255,175) ${x}% , rgb(24,23,31) ${x}%)`;
+  this.style.background = color;
+  // value change
+  sliderVal.textContent = this.value;
+});
